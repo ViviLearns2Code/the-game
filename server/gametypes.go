5dae023d57bf64b27d10e722c68946d224767a72
vivi <vivilearns2code@github.com>
2020-12-20 20:05:23 +0100

    Remove omitempty

--- server/gametypes.go
+++ server/gametypes.go
@@ -6,17 +6,17 @@ import (
 
 // public for automatic test
 type InputDetails struct {
-	GameToken    string `json:"gameToken,omitempty"`
-	PlayerToken  string `json:"playerToken,omitempty"`
-	PlayerName   string `json:"playerName,omitempty"`
-	PlayerIconId int    `json:"playerIconId,omitempty"`
-	ActionId     string `json:"actionId,omitempty"`
-	CardId       int    `json:"cardId,omitempty"`
+	GameToken    string `json:"gameToken"`
+	PlayerToken  string `json:"playerToken"`
+	PlayerName   string `json:"playerName"`
+	PlayerIconId int    `json:"playerIconId"`
+	ActionId     string `json:"actionId"`
+	CardId       int    `json:"cardId"`
 }
 
 type GameOutput struct {
 	ErrorMsg  string     `json:"errorMsg"`
-	GameState *GameState `json:"gameState,omitempty"`
+	GameState *GameState `json:"gameState"`
 }
 
 // structs for game core
@@ -72,51 +72,51 @@ type Game struct {
 }
 
 type CardsOnTable struct {
-	TopCard int `json:"topCard,omitempty"`
-	Level   int `json:"level,omitempty"`
-	Lives   int `json:"lives,omitempty"`
-	Stars   int `json:"stars,omitempty"`
+	TopCard int `json:"topCard"`
+	Level   int `json:"level"`
+	Lives   int `json:"lives"`
+	Stars   int `json:"stars"`
 }
 
 type CardsOfPlayer struct {
-	CardsInHand           []int       `json:"cardsInHand,omitempty"`
-	NrCardsOfOtherPlayers map[int]int `json:"nrCardOfOtherPlayers,omitempty"`
+	CardsInHand           []int       `json:"cardsInHand"`
+	NrCardsOfOtherPlayers map[int]int `json:"nrCardOfOtherPlayers"`
 }
 
 type ReadyEvent struct {
 	// lobby, concentrate
-	Name string `json:"name,omitempty"`
+	Name string `json:"name"`
 	// playerId, 0 if lobby
-	TriggeredBy int `json:"triggeredBy,omitempty"`
+	TriggeredBy int `json:"triggeredBy"`
 	// playerId
-	Ready []int `json:"ready,omitempty"`
+	Ready []int `json:"ready"`
 }
 
 type PlaceCardEvent struct {
 	// placeCard, useStar
-	Name string `json:"name,omitempty"`
+	Name string `json:"name"`
 	// playerId, 0 if useStar
-	TriggeredBy int `json:"triggeredBy,omitempty"`
+	TriggeredBy int `json:"triggeredBy"`
 	// playerId to card number
-	DiscardedCard map[int][]int `json:"discardedCard,omitempty"`
+	DiscardedCard map[int][]int `json:"discardedCard"`
 }
 
 type ProcessStarEvent struct {
 	// proposeStar, agreeStar, rejectStar
-	Name        string `json:"name,omitempty"`
-	TriggeredBy int    `json:"triggeredBy,omitempty"`
-	ProStar     []int  `json:"proStar,omitempty"`
-	ConStar     []int  `json:"conStar,omitempty"`
+	Name        string `json:"name"`
+	TriggeredBy int    `json:"triggeredBy"`
+	ProStar     []int  `json:"proStar"`
+	ConStar     []int  `json:"conStar"`
 }
 
 type GameStateEvent struct {
 	// gameOver, lostLife, levelUp
-	Name          string `json:"name,omitempty"`
-	LevelTitle    string `json:"levelTitle,omitempty"`
-	StarsIncrease bool   `json:"starsIncrease,omitempty"`
-	StarsDecrease bool   `json:"starsDecrease,omitempty"`
-	LivesIncrease bool   `json:"livesIncrease,omitempty"`
-	LivesDecrease bool   `json:"livesDecrease,omitempty"`
+	Name          string `json:"name"`
+	LevelTitle    string `json:"levelTitle"`
+	StarsIncrease bool   `json:"starsIncrease"`
+	StarsDecrease bool   `json:"starsDecrease"`
+	LivesIncrease bool   `json:"livesIncrease"`
+	LivesDecrease bool   `json:"livesDecrease"`
 }
 
 // GameState per Player
@@ -126,14 +126,14 @@ type GameState struct {
 	PlayerName    string         `json:"playerName"`
 	PlayerId      int            `json:"PlayerId"`
 	PlayerIconId  int            `json:"PlayerIconId"`
-	CardsOfPlayer CardsOfPlayer  `json:"cardsOfPlayer,omitempty"`
-	PlayerIconIds map[int]int    `json:"playerIconIds,omitempty"`
-	PlayerNames   map[int]string `json:"playerNames,omitempty"`
-	CardsOnTable  CardsOnTable   `json:"cardsOnTable,omitempty"`
+	CardsOfPlayer CardsOfPlayer  `json:"cardsOfPlayer"`
+	PlayerIconIds map[int]int    `json:"playerIconIds"`
+	PlayerNames   map[int]string `json:"playerNames"`
+	CardsOnTable  CardsOnTable   `json:"cardsOnTable"`
 	// events
-	GameStateEvent   `json:"gameStateEvent,omitempty"`
-	ReadyEvent       `json:"readyEvent,omitempty"`
-	PlaceCardEvent   `json:"placeCardEvent,omitempty"`
-	ProcessStarEvent `json:"processStarEvent,omitempty"`
+	GameStateEvent   `json:"gameStateEvent"`
+	ReadyEvent       `json:"readyEvent"`
+	PlaceCardEvent   `json:"placeCardEvent"`
+	ProcessStarEvent `json:"processStarEvent"`
 	err              *gameError
 }
